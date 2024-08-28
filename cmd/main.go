4226226937871191ef5097e82ecbfd8eb8eb0e22
Mozart <mozart@toteduca.com>
2024-08-28 08:30:09 -0300

    feat: initialize connection with database

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/mozart-rue/go-ecomerce/cmd/api"
+	"github.com/mozart-rue/go-ecomerce/config"
+	"github.com/mozart-rue/go-ecomerce/db"
 )
 
 func main() {
-  server := api.NewApiServer(":8080", nil)
-  if err := server.Run(); err != nil {
-    log.Fatal(err)
-  }
+	db, err := db.NewMySqlStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		fmt.Println(" Erro ao connectar ao banco")
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewApiServer(":8080", db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		fmt.Println(" Erro ao fazer PING ao banco")
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected")
 }
