b0e11f18361f4668303393196df9f2aea061590d
Mozart <mozart@toteduca.com>
2024-09-03 08:05:05 -0300

    feat: start implemente de route handler to create user

--- services/user/routes.go
+++ services/user/routes.go
@@ -4,18 +4,20 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/mozart-rue/go-ecomerce/types"
+	"github.com/mozart-rue/go-ecomerce/utils"
 )
 
 type Handler struct {
 }
 
 func NewHandler() *Handler {
-  return &Handler{}
+	return &Handler{}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
-  router.HandleFunc("/login", h.handleLogin).Methods("POST")
-  router.HandleFunc("/register", h.handleRegister).Methods("POST")
+	router.HandleFunc("/login", h.handleLogin).Methods("POST")
+	router.HandleFunc("/register", h.handleRegister).Methods("POST")
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
@@ -23,5 +25,12 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJson(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 
+	// check if user exists
+	// if it doesn't create new user
 }
