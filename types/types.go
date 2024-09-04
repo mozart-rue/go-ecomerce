bcf83f1913cb30047ae82c5e0bfeb36526236811
Mozart <mozart@toteduca.com>
2024-09-04 07:55:41 -0300

    feat: add user type

--- types/types.go
+++ types/types.go
@@ -1,7 +1,20 @@
 package types
 
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+}
+
+type User struct {
+	Id        int    `json:"id"`
+	LastName  string `json:"lastName"`
+	FirstName string `json:"firstName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"createdAt"`
+}
+
 type RegisterUserPayload struct {
-	FirstName string `json:"firstName`
+	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
